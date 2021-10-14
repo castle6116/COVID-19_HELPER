332b0c64554eef52c5faf2585b806cb2eadb0c5b
castle6116 <osc2143@gmail.com>
2021-10-14 17:58:22 +0900

    first commit

---
+++ COVID-19_HELPER/Model/Covid.swift
@@ -0,0 +1,8 @@
+//
+//  Covid.swift
+//  COVID-19_HELPER
+//
+//  Created by 김진우 on 2021/10/11.
+//
+
+import Foundation
---
+++ COVID-19_HELPER/View/SideMenuTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SideMenuTableViewCell.swift
+//  COVID-19_HELPER
+//
+//  Created by 김진우 on 2021/10/11.
+//
+
+import UIKit
+
+class SideMenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
